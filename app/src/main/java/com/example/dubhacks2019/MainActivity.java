270c9ef5210f053753f4071914af8223e1e0530b
Justin <justinkuan2000@gmail.com>
2019-10-12 16:51:39 -0700

    commiting before pulling from sonny's branch

--- app/src/main/java/com/example/dubhacks2019/MainActivity.java
+++ app/src/main/java/com/example/dubhacks2019/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.dubhacks2019;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -17,14 +18,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
+
+//    @Override
+////    protected void onStart () {
+////
+////        TextView textView = (TextView) findViewById(R.id.textView);
+////        textView.setText("You're Gay"); //set text for text view
+////
+////    }
 
 }
