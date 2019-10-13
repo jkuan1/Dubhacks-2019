dee8cbc0c697a0d7d782f868c2f145c3414e996c
dkjklmn <lin.ryan99@hotmail.com>
2019-10-13 01:08:34 -0700

    1:08 am Settings Button now does stuff

---
+++ app/src/main/java/com/example/dubhacks2019/SettingsActivity.java
@@ -0,0 +1,32 @@
+package com.example.dubhacks2019;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_settings);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
