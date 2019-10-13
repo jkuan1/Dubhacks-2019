cd9f234bcd5efcf6204fc991ff7dccbdac2e2190
dkjklmn <lin.ryan99@hotmail.com>
2019-10-13 10:53:29 -0700

    10:52 am Pressing Confirm now stores the words as strings

--- app/src/main/java/com/example/dubhacks2019/AddGoalActivity.java
+++ app/src/main/java/com/example/dubhacks2019/AddGoalActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,6 +22,14 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class AddGoalActivity extends AppCompatActivity{
+
+    EditText NewGoal;
+    EditText Description;
+    EditText GoalLength;
+    TextView NewGoalText;
+    TextView DescriptionText;
+    TextView GoalLengthText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,9 +39,20 @@ public class AddGoalActivity extends AppCompatActivity{
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(AddGoalActivity.this, MainActivity.class));
+                NewGoal   = (EditText)findViewById(R.id.NewGoal);
+                String Goal = NewGoal.getText().toString();
+
+                Description   = (EditText)findViewById(R.id.Description);
+                String Desc = Description.getText().toString();
+
+                GoalLength   = (EditText)findViewById(R.id.GoalLength);
+                String Goallen = GoalLength.getText().toString();
+
             }
         });
         }
+
+
     @Override
     protected void onStart() {
         super.onStart();
