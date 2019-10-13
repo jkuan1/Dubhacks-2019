abfaddd601e7e3b7d34756d1e3d6d65b53419f0c
dkjklmn <lin.ryan99@hotmail.com>
2019-10-13 09:13:30 -0700

    9:13 am Goal Screen Image + Text

--- app/src/main/java/com/example/dubhacks2019/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/dubhacks2019/ui/dashboard/DashboardFragment.java
@@ -37,13 +37,13 @@ public class DashboardFragment extends Fragment {
 
         });
 
- /*       final Button goals_button = root.findViewById(R.id.add_goal_button);
+        final Button goals_button = root.findViewById(R.id.add_goal_button);
         goals_button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 Intent i = new Intent(getActivity(), AddGoalActivity.class);
                 startActivity(i);
             }
-        }); */
+        });
 
         return root;
     }
